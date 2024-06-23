c2f28f89f985cd404891c9d0a4d459b66d9ca29c
lukblan <luk.blanko@gmail.com>
2024-06-23 20:52:14 -0300

    Add second products line

--- src/component/ModuleHome/ModuleHome.jsx
+++ src/component/ModuleHome/ModuleHome.jsx
@@ -3,6 +3,28 @@ import {useEffect, useState} from "react";
 import {fetchProducts} from "../../services/fetch-products.js";
 import {ProductLine} from "../Utils/ProductLine/ProductLine.jsx";
 
+function getLines(products, linesAmount, lineSize) {
+  const allLines = []
+  let line = []
+  let currentIndex = 0;
+
+  for (let i = 0; i < linesAmount; i++) {
+    allLines.push({line: [], code: i})
+  }
+
+  for (let i = 0; i < products.length; i++) {
+    line.push(products[i])
+
+    if ((i + 1) % lineSize === 0) {
+      allLines[currentIndex].line = line;
+      line = []
+      currentIndex += 1
+    }
+  }
+
+  return allLines;
+}
+
 function useProducts() {
   const [products, setProducts] = useState([])
 
@@ -15,11 +37,12 @@ function useProducts() {
 
 function ModuleHome() {
   const { products } = useProducts();
-  const firstTenProducts = products.filter((product, index) => index < 10);
 
+  const allLines = getLines(products, 2, 10)
+  console.log(allLines)
   return (
     <main className="main-screen-container">
-      <ProductLine products={firstTenProducts}/>
+      {allLines.map(line => <ProductLine key={line.code} products={line.line} />)}
     </main>
   )
 }

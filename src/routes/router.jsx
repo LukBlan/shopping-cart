92533bc5bd77809e0a932f9cb0f92144b866e03f
lukblan <luk.blanko@gmail.com>
2024-06-25 20:57:04 -0300

    Add param route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,16 +1,31 @@
 import {ModuleHome} from "../component/ModuleHome/ModuleHome.jsx";
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {WebsiteLayout} from "../layout/WebsiteLayout.jsx";
-import {ModuleShop} from "../component/ModuleShop/ModuleShop.jsx";
+import { ModuleShop } from "../component/ModuleShop/ModuleShop.jsx";
 
 const routes = [
-  {path: "/", element: <ModuleHome />, nav: false},
+  {
+    path: "/",
+    element: <ModuleHome />, nav: false,
+    children: [{
+      path: ":id",
+      element: <div>test</div>
+    }]
+  },
   {path: "Shop", element: <ModuleShop />, nav: true},
 ]
 
 const mapRoutes = createRoutesFromElements(
   <Route element={<WebsiteLayout />} >
-    {routes.map(route => <Route key={route.path} path={route.path} element={route.element} />)}
+    {routes.map(route =>
+      <Route key={route.path} path={route.path} element={route.element} >
+        {
+          route.children ?
+            route.children.map(childrenRoute => <Route key={childrenRoute.path} path={childrenRoute.path} element={childrenRoute.element}/>) :
+            <></>
+        }
+      </Route>)
+    }
   </Route>
 )
 

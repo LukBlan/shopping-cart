6660970066f534ba56e03b3188dced858599e755
lukblan <luk.blanko@gmail.com>
2024-06-27 18:15:49 -0300

    Remove footer

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,13 +1,11 @@
 import {Nav} from "../components/Nav/Nav.jsx";
 import {Outlet} from "react-router-dom";
-import {Footer} from "../components/Footer/Footer.jsx";
 
 function Root() {
   return (
     <>
       <Nav />
       <Outlet />
-      <Footer />
     </>
   )
 }

c961e5fc6bd5232ee8a593e611d73744aad70db5
lukblan <luk.blanko@gmail.com>
2024-06-27 19:04:26 -0300

    Add key to fragment

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import "./Nav.css"
 import { Link, NavLink } from "react-router-dom";
 import { routes } from "../../router/router.jsx";
 import { SvgCart } from "../Svgs/SvgCart.jsx";
+import {Fragment} from "react";
 
 function Nav() {
   return (
@@ -13,9 +14,9 @@ function Nav() {
       <div className="shopping-elements-container">
         <ul className="nav-links">
           {routes.map(
-            route => (route.nav) ?
-              <li key={route.path}><NavLink className="link" to={route.path}>{route.path}</NavLink></li> :
-              <></>
+            (route, index) => (route.nav) ?
+              <li key={index}><NavLink className="link" to={route.path}>{route.path}</NavLink></li> :
+              <Fragment key={index}></Fragment>
           )}
         </ul>
 

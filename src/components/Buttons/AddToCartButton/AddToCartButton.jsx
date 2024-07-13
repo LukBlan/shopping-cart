ccfc3c23c2111c2de7bbd7595b310f006cf0a5cc
lukblan <luk.blanko@gmail.com>
2024-07-12 22:47:05 -0300

    Add styles to price tag

--- src/components/Buttons/AddToCartButton/AddToCartButton.jsx
+++ src/components/Buttons/AddToCartButton/AddToCartButton.jsx
@@ -3,7 +3,7 @@ import "./AddToCart.css"
 import {useOutletContext} from "react-router-dom";
 
 function AddToCartButton({id}) {
-  const [products, addToCart] = useOutletContext();
+  const [, addToCart] = useOutletContext();
 
   return (
     <button className="add-cart-button" onClick={(event) => {
--- src/components/Cards/ProductCard/ProductCard.jsx
+++ src/components/Cards/ProductCard/ProductCard.jsx
@@ -6,11 +6,12 @@ function ProductCard({product}) {
   return (
     <li className="product-card">
       <h2 className="product-card-title">{title}</h2>
-      <p>{price}</p>
 
       <div className="product-card-image-container">
         <img className="product-card-image" src={image} alt={title}/>
       </div>
+
+      <p className="product-tag">{price} $</p>
     </li>
   )
 }
